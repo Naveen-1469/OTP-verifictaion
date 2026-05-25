6b89ae6ab6df8eae9e61eaafb85fab79eb8098f5
Naveen-1469 <your-naveenm1469@gmail.com>
2026-05-25 23:57:58 +0530

    update project

---
+++ script.js
@@ -0,0 +1,68 @@
+//dropdown functionallity
+const dropdownbtn = document.querySelector(".dropdown-btn");
+const dropdownContainer = document.querySelector(".dropdown-container");
+dropdownbtn.addEventListener("click", () => {
+  dropdownContainer.style.display =
+    dropdownContainer.style.display === "block" ? "none" : "block";
+});
+
+//OTP input auto focus
+
+const inputs = document.querySelector(".otp-inputs input");
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    if (e.target.value.lenght === 1) {
+      if (index < inputs.lenght - 1) {
+        inputs[index + 1].focus();
+      }
+    }
+  });
+});
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Backspace" && e.target.value.lenght === 0) {
+    if (index > 0) {
+      inputs[index - 1].focus();
+    }
+  }
+});
+
+//Resend OTP timer
+let timeLeft = 30;
+const timerElement = document.getElementById("timer");
+const resendBtn = document.getElementById("resendBtn");
+resendBtn.disabled = true;
+
+const countdown = setInterval(() => {
+  timeLeft--;
+  timerElement.textContent = `Resend in ${timeLeft}s`;
+  if (timeLeft < 0) {
+    clearInterval(countdown);
+    timerElement.textContent = "";
+    resendBtn.disabled = false;
+  }
+}, 1000);
+
+resendBtn.addEventListener("click", () => {
+  alert("OTP Resent");
+  timeLeft = 30;
+  resendBtn.disabled = true;
+  const newCountdown = setInterval(() => {
+    timeLeft--;
+    timerElement.textContent = `Resend in ${timeLeft}s`;
+    if (timeLeft <= 0) {
+      clearInterval(newCountdown);
+      timerElement.textContent = "";
+      resendBtn.disabled = false;
+    }
+  }, 1000);
+});
+
+//form submission
+
+document.getElementById("otp-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const otp = Array.from(inputs)
+    .map((input) => input.value)
+    .join("");
+  alert(`OTP entered: ${otp}`);
+});
